b2d035f11c4a951516600e432cb6cc624663a74a
Stuart ALLEN <sallen@e5r5p3.jnb.42.fr>
2016-11-23 14:53:22 +0200

    ray direction transf matrix initd

--- srcs/render.c
+++ srcs/render.c
@@ -3,8 +3,21 @@
 void			draw(t_env *env)
 {
 	int x = -1;
-	while (++x < WIDTH) //This loops is what produces the red line across the screen.
+	while (++x < WIDTH) //This loop is what produces the red line across the screen.
 		put_pixel(env, x, 12, 0xFF0000);
+
+
+	/*
+		Calculate direction of ray relative to position of player and camera coords
+	*/
+
+	env->camera.pos_x = 2 * x / double(WIDTH) - 1;
+	env->ray.pos_x = env->player.pos_x;
+	env->ray.pos_y = env->player.pos_y;
+	env->ray.dir_x = env->player.dir_x + env->player.plane_x * env->camera.pos_x;
+	env->ray.dir_y = env->player.dir_y + env->player.plane_y * env->camera.pos_x;
+
+
 	mlx_put_image_to_window(env->mlx, env->win, env->img, 0, 0);
 }
 
